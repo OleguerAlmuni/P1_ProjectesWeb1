b39e63359d9f955a6360bcb8188e8d7131eb2d99
Àlex <alex.bayona@students.salle.url.edu>
2023-10-28 19:35:33 +0200

    Canvis a App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
     import Header from './components/Header.vue'
     import Log from './components/Log.vue'
+    import CreateAccount from './components/CreateAccount.vue'
     import Footer from './components/Footer.vue'
 </script>
 
 <template>
     <Header />
 
+    <title>GAME TITLE</title>
     <router-link to="/LogIn">LogIn</router-link>
+    <router-link to "/CreateAccount">Create Account</router-link>
 
     <Footer />
 </template>
