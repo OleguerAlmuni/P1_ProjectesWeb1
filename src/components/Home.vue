708162f0ec74eed665483c282d7315bff109cca4
Àlex <alex.bayona@students.salle.url.edu>
2023-10-28 20:11:23 +0200

    Commit per fer pull

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { ref } from 'vue'
+</script>
+
+<template>
+    <body>
+        <div class="home">
+            <button type="button" class="playNowButton">PLAY NOW</button>
+            <br><br>
+            <h2 class="center_text">Ranking players:</h2>
+            <ol>
+                <li>Player 01</li>
+                <li>Player 66</li>
+                <li>Player 96</li>
+            </ol>
+        </div>
+    </body>
+</template>
