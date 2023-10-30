a45359eed7b3a2ff7c29b1280b5ab9d32b22168a
Àlex <alex.bayona@students.salle.url.edu>
2023-10-30 12:35:46 +0100

    Últim canvi amb l'Oleguer :3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
     import Header from './components/Header.vue'
     import Footer from './components/Footer.vue'
-    import CreateAccount from './components/CreateAccount.vue'
-    import Home from './components/Home.vue'
 
 </script>
 
 <template>
     <Header />
-
-    <CreateAccount />
-    <Home />
 
     <nav>
         <button><router-link to="/login">Log</router-link></button>
