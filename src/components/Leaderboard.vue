066697bf13691422b17edd36a40e9987d75b3d68
Miquel <miquel.barbara@students.salle.url.edu>
2023-11-02 19:29:48 +0100

    attack

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,9 +1,11 @@
 <script setup>
     import { ref } from 'vue'
+    import Header from '../components/Header.vue'
 </script>
 
 
 <template>
+    <header></header>
     <div>
         <table>
             <tr>
