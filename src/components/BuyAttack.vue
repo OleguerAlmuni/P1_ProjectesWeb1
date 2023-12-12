8802d83c4769adc1f29574fc07803a688a6149cc
Miquel <miquel.barbara@students.salle.url.edu>
2023-12-12 16:47:23 +0100

    vull fer pull uwu

--- src/components/BuyAttack.vue
+++ src/components/BuyAttack.vue
@@ -7,7 +7,8 @@
     export default {
         data() {
             return {
-                attackData: []
+                attackData: [],
+                response: ""
             };
         },
         mounted() {
--- src/components/CreateAttack.vue
+++ src/components/CreateAttack.vue
@@ -21,7 +21,6 @@
                 fetch("http://balandrau.salle.url.edu/shop/attacks", {
                     method: 'POST',
                     headers: {
-                        'Authorization': 'Bearer ${token}',
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(attack)
